0a5922cd1a5a179e4b68dd52c9a7515bf1553404
Vitor Wuchryn <wuchryn@proton.me>
2023-11-15 11:28:34 -0300

    Update zfs-bench.sh
    benchmark zfs pool test-l2arc-cache

--- zfs-bench.sh
+++ zfs-bench.sh
@@ -2,16 +2,16 @@
 
 echo $(date):Random read
 fio --filename=/test-l2arc-cache/test --sync=1 --rw=randread --bs=4k --numjobs=1 \
-  --iodepth=4 --group_reporting --name=test --filesize=10G --runtime=300 && rm test
+  --iodepth=4 --group_reporting --name=test --filesize=10G --runtime=300 && rm /test-l2arc-cache/test
 
 echo $(date):Random write
 fio --filename=/test-l2arc-cache/test --sync=1 --rw=randwrite --bs=4k --numjobs=1 \
-  --iodepth=4 --group_reporting --name=test --filesize=10G --runtime=300 && rm test
+  --iodepth=4 --group_reporting --name=test --filesize=10G --runtime=300 && rm /test-l2arc-cache/test
 
 echo $(date):Sequential read
 fio --filename=/test-l2arc-cache/test --sync=1 --rw=read --bs=4k --numjobs=1 \
-  --iodepth=4 --group_reporting --name=test --filesize=10G --runtime=300 && rm test
+  --iodepth=4 --group_reporting --name=test --filesize=10G --runtime=300 && rm /test-l2arc-cache/test
 
 echo $(date):Sequential write
 fio --filename=/test-l2arc-cache/test --sync=1 --rw=write --bs=4k --numjobs=1 \
-  --iodepth=4 --group_reporting --name=test --filesize=10G --runtime=300 && rm test
+  --iodepth=4 --group_reporting --name=test --filesize=10G --runtime=300 && rm /test-l2arc-cache/test
